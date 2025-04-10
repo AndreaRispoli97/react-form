5c443346b20fd22df99ecbb379cce304aa33791b
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-10 10:25:12 +0200

    fix: structure form and style css

--- src/App.jsx
+++ src/App.jsx
@@ -25,19 +25,19 @@ function App() {
       <div className='head'>
         <h1>Lista Manga</h1>
       </div>
+
+
+      <ul>
+        {mangaArray.map((manga, index) =>
+          <li key={index}>
+            <h3>{manga}</h3>
+            <i className="fa-solid fa-trash" type='button' onClick={() => deleteManga(index)}> Delete Manga</i>
+          </li>)
+        }
+
+      </ul>
+      <hr />
       <form onSubmit={addManga}>
-
-        <ul>
-          {mangaArray.map((manga, index) =>
-            <li key={index}>
-              <h3>{manga}</h3>
-              <i className="fa-solid fa-trash" type='button' onClick={() => deleteManga(index)}> Delete Manga</i>
-            </li>)
-          }
-
-        </ul>
-        <hr />
-
         <input type="text"
           value={newManga}
           // placeholder='Aggiungi'
